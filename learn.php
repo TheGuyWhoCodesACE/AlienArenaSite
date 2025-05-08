b6be15b9b22bd56867a1212dc00e7369a5a5f467
TheGuyWhoCodesACE <andrewe392@gmail.com>
2025-05-07 21:09:16 -0700

    Add files via upload

---
+++ learn.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Learn Alien Gambits</title>
+</head>
+<body>
+  <h2>Learn Alien Gambits</h2>
+  <ul>
+    <li><a href="alien_mainline.html">Alien Gambit Chessable Course</a></li>
+    <li><a href="bonjour_variation.html">Witty Alien Youtube Video</a></li>
+    <li><a href="viking_variation.html">GothamChess Youtube Video</a></li>
+    <li><a href="martian_gambit.html">Martian Gambit Youtube video</a></li>
+    <li><a href="martian_gambit.html">Bonjour Variation Youtube video</a></li>
+    <li><a href="martian_gambit.html">Viking Variation Youtube video</a></li>
+  </ul>
+</body>
+</html>
